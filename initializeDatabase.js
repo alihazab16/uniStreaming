6eed37565412ee4ac96f8b5888fb019edec477fd
ali azab <alih.azab16@gmail.com>
2026-04-08 12:59:10 -0400

    Update MongoDB connection URL to use environment variable

--- initializeDatabase.js
+++ initializeDatabase.js
@@ -6,7 +6,7 @@ const { MongoClient } = require("mongodb");
 const fs = require("fs");
 const path = require("path");
 
-const MONGO_URL = "mongodb://localhost:27017";
+const MONGO_URL = "mongoose.connect(process.env.MONGO_URI);";
 const DB_NAME = "mwp"; // This is the name of our database
 const SERVICES_DIR = path.join(__dirname, "streamingServices");
 
